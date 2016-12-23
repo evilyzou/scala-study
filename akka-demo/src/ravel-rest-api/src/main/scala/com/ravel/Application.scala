e99b253884a3a09c8a34a74d9fd9cd7574119ff4
CloudZou <Cloud.Zou@software.dell.com>
2016-12-23 15:55:27 +0800

    s

--- akka-demo/src/ravel-rest-api/src/main/scala/com/ravel/Application.scala
+++ akka-demo/src/ravel-rest-api/src/main/scala/com/ravel/Application.scala
@@ -1,14 +1,36 @@
 package com.ravel
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorLogging, ActorSystem, Props}
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+import com.ravel.RestInterface._
 import com.ravel.actors.Starter
-import com.ravel.{Config => c}
+import com.ravel.{Config => C}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.StdIn
+
 /**
  * Created by CloudZou on 12/12/16.
  */
-object Application extends App{
-  val system = ActorSystem("ravel-app")
+object Application extends App {
+//  val system = ActorSystem("ravel-app")
+//
+//  val starter = system.actorOf(Props[Starter], name = "main")
+//  starter ! Starter.Start
+  implicit val system = ActorSystem("my-system")
 
-  val starter = system.actorOf(Props[Starter], name = "main")
-  starter ! Starter.Start
+  import akka.stream.ActorMaterializer
+  implicit val materializer = ActorMaterializer()
+
+  Http().bindAndHandle(routes, C.host, C.port) map { binding =>
+    C.log.info(s"REST interface bound to ${binding.localAddress}")
+
+    StdIn.readLine() // let it run until user presses return
+    binding.unbind().onComplete(e =>{
+      C.log.error("error occured")
+      system.terminate()
+    } )
+  } recover {
+    case ex => C.log.info(s"REST interface could not bind to $C.host:$C.port", ex.getMessage)
+  }
 }
