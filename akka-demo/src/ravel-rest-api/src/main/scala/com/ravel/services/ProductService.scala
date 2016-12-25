de4f9f8310e9c12f2549aa843098fc1f0317c1a8
CloudZou <409492828@qq.com>
2016-12-25 12:34:46 +0800

    s

--- akka-demo/src/ravel-rest-api/src/main/scala/com/ravel/services/ProductService.scala
+++ akka-demo/src/ravel-rest-api/src/main/scala/com/ravel/services/ProductService.scala
@@ -22,7 +22,7 @@ object ProductService{
     val searchFuture = esClient.execute {
       search in esIndex / esTypeProduct query {
 //        termQuery("pfunction", filter.pfunction)
-        termQuery("systemType", "SystemOther")
+        termQuery("systemType", "systemother")
 //        matchQuery("customType", filter.customType)
       } start(start) limit(filter.size)
     }
